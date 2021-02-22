02fb6c588e167552582c53a81a02b9425ddbe359
chenyj <2932802684@qq.com>
2021-02-22 17:26:50 +0800

    feat: update version

--- packages/mysql-mybatis/types/index.d.ts
+++ packages/mysql-mybatis/types/index.d.ts
@@ -2,7 +2,20 @@ export type MYBATIS_SQL =
   | string
   | ((
       data: any,
-      operator: { trim; where; set; choose }
+      operator: {
+        trim: (
+          param: {
+            prefix?: string | undefined
+            prefixOverrides?: string | string[] | undefined
+            suffix?: string | undefined
+            suffixOverrides?: string | string[] | undefined
+          },
+          ...statements: Array<string | undefined | null>
+        ) => string
+        where: (...statements: Array<string | undefined | null>) => string
+        set: (...statements: Array<string | undefined | null>) => string
+        choose: (...statements: Array<string | undefined | null>) => string
+      }
     ) => string | Array<string | undefined | null>)
 
 export function Select(sql: MYBATIS_SQL): MethodDecorator
